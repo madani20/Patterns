c1c5923a83e3fdc305e5920afa5b93c8e21b5efe
Madani <khouane.madani@free.fr>
2022-09-02 15:54:00 +0200

    finalisé Adapter

--- src/main/java/adapter/PlugingRCA.java
+++ src/main/java/adapter/PlugingRCA.java
@@ -3,6 +3,6 @@ package adapter;
 public class PlugingRCA implements Plug{
     @Override
     public void plugRCA() {
-        System.out.println("prise RCA branché");
+        System.out.println("prise RCA adapté ... ");
     }
 }
