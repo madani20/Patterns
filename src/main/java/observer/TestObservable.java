ad13778e900ba28efa356e7122455acad7e3ba59
Madani <47392318+madani20@users.noreply.github.com>
2025-05-23 17:13:44 +0200

    Update TestObservable.java

--- src/main/java/observer/TestObservable.java
+++ src/main/java/observer/TestObservable.java
@@ -6,29 +6,7 @@ import java.util.stream.Stream;
 
 public class TestObservable {
 
-    public static void f1() {
-        Stream.of("Hello")
-                .forEach(System.out::println);
-    }
-
-    public static void f2(int limit) {
-        Stream.iterate(1, n -> n + 1)
-                .limit(limit)
-                .forEach(System.out::println);
-    }
-
-    public static int f3(int[] tab) {
-        return Arrays.stream(tab).max().getAsInt();
-    }
-
-    public static long f4(String str) {
-        return str.chars()
-                .filter(c -> Pattern.matches("[aoeiyu]", Character.toString(c)))
-                .count();
-    }
-
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         ObservableImpl observable = new ObservableImpl();
         Observer observer1 = new ObserverImpl1();
         Observer observer2 = new ObserverImpl2();
